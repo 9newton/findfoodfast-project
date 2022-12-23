975b16f83ea56318fe53adbadaf7575b55307c9c
Saranyu <saranyumax761@gmail.com>
2022-12-23 19:56:21 +0700

    commit home

--- front-end/src/components/Home/Home.js
+++ front-end/src/components/Home/Home.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import './Home.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Card from 'react-bootstrap/Card';
+import Footer from '../Footer';
+import Header from '../Header';
 
 function HomePage() {
     return (
         <div className="home">
-            <h1>EIEIEI2</h1>
-            <div class="card">
-                <div class="card-body">
-                    This is some text within a card body.
-                </div>
-                <div>ajfafadfdf</div>
-            </div>
+            <Header />
+            {/* <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+    <Footer /> */}
         </div>
+        
     )
 }
 
