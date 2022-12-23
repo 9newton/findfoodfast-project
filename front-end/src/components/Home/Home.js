cb90aa6706c5a53274aafc4ad50ce710e1fccf24
Pakorn Khunleang <9newton@gmail.com>
2022-12-23 17:55:12 +0700

    commit first

---
+++ front-end/src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function HomePage() {
+    return (
+        <div className="home">
+            <h1>Home Page</h1>
+            <div class="card">
+                <div class="card-body">
+                    This is some text within a card body.
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
