02640ce8da60b73a64a9ea8e8bbe34d7514ddf9c
Saranyu <saranyumax761@gmail.com>
2022-12-25 22:53:28 +0700

    commit admin

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Home from './components/Home/Home';
 import Random from './components/Random/Random';
 import Report from './components/Report/Report';
-
+import Admin from './components/Admin/Admin';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Admin />
       {/* <Random /> */}
       {/* <Report /> */}
     </div>
