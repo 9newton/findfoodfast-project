8eb2d5155111e7bb53eb88909ef9966eb2e36afa
Pakorn Khunleang <9newton@gmail.com>
2023-01-03 23:26:06 +0700

    add firebase to backend

--- back-end/controllers/RestaurantController.js
+++ back-end/controllers/RestaurantController.js
@@ -1,11 +1,19 @@
 import Restaurant from "../models/RestaurantModel.js";
+import serviceAccount from '../firebase-config.json' assert { type: "json" };
+import admin from 'firebase-admin';
+const FirebaseApp = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "gs://fff-project-95ee1.appspot.com"
+});
+const storage = FirebaseApp.storage();
+const bucket = storage.bucket();
 
 export const getRestaurants = async (req, res) => {
     try {
         const restaurants = await Restaurant.find();
         res.json(restaurants);
     } catch (error) {
-        res.status(500).json({message: error.message});
+        res.status(500).json({ message: error.message });
     }
 }
 
@@ -14,7 +22,7 @@ export const getRestaurantById = async (req, res) => {
         const restaurant = await Restaurant.findById(req.params.id);
         res.json(restaurant);
     } catch (error) {
-        res.status(404).json({message: error.message});
+        res.status(404).json({ message: error.message });
     }
 }
 
@@ -24,24 +32,49 @@ export const saveRestaurant = async (req, res) => {
         const insertedrestaurant = await restaurant.save();
         res.status(201).json(insertedrestaurant);
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(400).json({ message: error.message });
     }
 }
 
 export const updateRestaurant = async (req, res) => {
     try {
-        const updatedrestaurant = await Restaurant.updateOne({_id:req.params.id}, {$set: req.body});
+        const updatedrestaurant = await Restaurant.updateOne({ _id: req.params.id }, { $set: req.body });
         res.status(200).json(updatedrestaurant);
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(400).json({ message: error.message });
     }
 }
 
 export const deleteRestaurant = async (req, res) => {
     try {
-        const deletedrestaurant = await Restaurant.deleteOne({_id:req.params.id});
+        const deletedrestaurant = await Restaurant.deleteOne({ _id: req.params.id });
         res.status(200).json(deletedrestaurant);
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(400).json({ message: error.message });
+    }
+}
+
+export const uploadImageRestaurant = async (req, res) => {
+    try {
+        const folder = 'restaurant1'
+        const fileName = `${folder}/${Date.now()}`
+        const fileUpload = bucket.file(fileName);
+        const blobStream = fileUpload.createWriteStream({
+            metadata: {
+                contentType: req.file.mimetype
+            }
+        });
+
+        blobStream.on('error', (err) => {
+            res.status(405).json(err);
+        });
+
+        blobStream.on('finish', () => {
+            res.status(200).send('Upload complete!');
+        });
+
+        blobStream.end(req.file.buffer);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
 }
--- back-end/routes/RestaurantRoute.js
+++ back-end/routes/RestaurantRoute.js
@@ -1,17 +1,28 @@
 import express from "express";
-import { 
-    getRestaurants, 
+import {
+    getRestaurants,
     getRestaurantById,
     saveRestaurant,
     updateRestaurant,
-    deleteRestaurant
+    deleteRestaurant,
+    uploadImageRestaurant
 } from "../controllers/RestaurantController.js";
+import Multer from 'multer';
 
 const router = express.Router();
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+});
+
+
 
 router.get('/restaurants', getRestaurants);
 router.get('/restaurants/:id', getRestaurantById);
 router.post('/restaurants', saveRestaurant);
+router.post('/restaurants/upload', multer.single('img'), uploadImageRestaurant);
 router.patch('/restaurants/:id', updateRestaurant);
 router.delete('/restaurants/:id', deleteRestaurant);
 
