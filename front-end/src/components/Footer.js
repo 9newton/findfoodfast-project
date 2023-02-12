235d5bf06697f03b988234cba6ecb3bd6d7aa05e
Saranyu <saranyumax761@gmail.com>
2023-02-12 21:10:36 +0700

    update footer

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,45 +1,63 @@
-import './Footer.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import Logo from '../image/Logo3.png';
-import { FaFacebook } from 'react-icons/fa';
+import "./Footer.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Logo from "../image/Logo3.png";
+import { FaFacebook } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Image from "react-bootstrap/Image";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs={12} col-md">
-                        <Link to='/'><Image src={Logo} /></Link>
-                        <ul className='list-unstyled'>
-                            <a href="https://www.facebook.com/FFFKlong6" className='icon-facebook' target={'_blank'} ><FaFacebook /></a>
-                        </ul>
-                    </div>
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs={12} col-md">
+            <Link to="/">
+              <Image src={Logo} />
+            </Link>
+            <ul className="list-unstyled">
+              <a
+                href="https://www.facebook.com/FFFKlong6"
+                className="icon-facebook"
+                target={"_blank"}
+              >
+                <FaFacebook />
+              </a>
+            </ul>
+          </div>
 
-                    <div className="col-xs={12} col-md">
-                        <h5>เกี่ยวกับเรา</h5>
-                        <ul className='list-unstyled'>
-                            <li>เว็ปไซต์ค้นหาร้านอาหารบริเวณ ทมร.ธัญบุรี จัดทำขึ้นมาเพื่อให้ผู้ที่อาศัยอยู่แถวคลองหกได้ใช้ เพื่อความสะดวกในการหาร้านอาหาร</li>
-                        </ul>
-                    </div>
+          <div className="col-xs={12} col-md">
+            <h5>เกี่ยวกับเรา</h5>
+            <ul className="list-unstyled">
+              <li>
+                เว็ปไซต์ค้นหาร้านอาหารบริเวณ ทมร.ธัญบุรี
+                จัดทำขึ้นมาเพื่อให้ผู้ที่อาศัยอยู่แถวคลองหกได้ใช้
+                เพื่อความสะดวกในการหาร้านอาหาร
+              </li>
+            </ul>
+          </div>
 
-                    <div className="col-xs={12} col-md">
-                        <h5>ช่วยเหลือ</h5>
-                        <ul className='list-unstyled'>
-                            <Link to='/report' className='a' style={{ textDecoration: 'none' }}><li>แจ้งปัญหา</li></Link>
-                        </ul>
-                    </div>
-                </div>
-                <hr />
-                <div className='row'>
-                    <div className='col-sm copyright'>
-                        &copy;{new Date().getFullYear()} FINDFOODFAST, ALL RIGHT RESERVED
-                    </div>
-                </div>
-            </div>
+          <div className="col-xs={12} col-md">
+            <h5>ช่วยเหลือ</h5>
+            <ul className="list-unstyled">
+              <Link
+                to="/report"
+                className="a"
+                style={{ textDecoration: "none" }}
+              >
+                <li>แจ้งข้อมูล</li>
+              </Link>
+            </ul>
+          </div>
         </div>
-    )
+        <hr />
+        <div className="row">
+          <div className="col-sm copyright">
+            &copy;{new Date().getFullYear()} FINDFOODFAST, ALL RIGHT RESERVED
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
